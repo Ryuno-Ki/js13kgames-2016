41b5a1d09237236756051a93905833e9b996c2cc
André Jaenisch <andre.jaenisch@openmailbox.org>
2016-08-29 20:50:37 +0200

    SVG has a different NS. createElementNS for XML node.

--- js/street.js
+++ js/street.js
@@ -11,18 +11,19 @@ class StreetView
     this.successor = successor || null
 
   render: () ->
-    el = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+    svgns = 'http://www.w3.org/2000/svg'
+    el = document.createElementNS(svgns, 'svg')
     el.setAttribute('viewBox', '0 0 100 100')
     el.setAttribute('version', '1.1')
     el.setAttribute('height', '60')
     el.setAttribute('width', '60')
 
-    g = document.createElement('g')
-    leftBoundary = document.createElement('path')
+    g = document.createElementNS(svgns'g')
+    leftBoundary = document.createElementNS(svgns, 'path')
     leftBoundary.setAttribute('d', 'M0 33H100')
-    rightBoundary = document.createElement('path')
+    rightBoundary = document.createElementNS(svgns, 'path')
     leftBoundary.setAttribute('d', 'M0 67H100')
-    middleBoundary = document.createElement('path')
+    middleBoundary = document.createElementNS(svgns, 'path')
     middleBoundary.setAttribute('d', 'M0 50H10')
 
     g.appendChild(leftBoundary)
